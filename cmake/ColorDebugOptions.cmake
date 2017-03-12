e58ddeaf4f6002ee17e5d53ce5758332c521c7c8
Bartek Łukawski <bwmn.peter@gmail.com>
2017-03-12 23:14:41 +0100

    Set COMPILE_DEFINITIONS property to main directory
    Needed for as-part-of-another-project usage because of the nested
    scope of add_definitions() (only current dir and subdirectories).

--- cmake/ColorDebugOptions.cmake
+++ cmake/ColorDebugOptions.cmake
@@ -2,26 +2,26 @@
 
 option(ColorDebug_HIDE_ERROR "Choose if you want to compile with CD_HIDE_ERROR" FALSE)
 if(ColorDebug_HIDE_ERROR)
-    add_definitions(-DCD_HIDE_ERROR)
+    set_property(DIRECTORY ${CMAKE_SOURCE_DIR} APPEND PROPERTY COMPILE_DEFINITIONS CD_HIDE_ERROR)
 endif()
 
 option(ColorDebug_HIDE_WARNING "Choose if you want to compile with CD_HIDE_WARNING" FALSE)
 if(ColorDebug_HIDE_WARNING)
-    add_definitions(-DCD_HIDE_WARNING)
+    set_property(DIRECTORY ${CMAKE_SOURCE_DIR} APPEND PROPERTY COMPILE_DEFINITIONS CD_HIDE_WARNING)
 endif()
 
 option(ColorDebug_HIDE_SUCCESS "Choose if you want to compile with CD_HIDE_SUCCESS" FALSE)
 if(ColorDebug_HIDE_SUCCESS)
-    add_definitions(-DCD_HIDE_SUCCESS)
+    set_property(DIRECTORY ${CMAKE_SOURCE_DIR} APPEND PROPERTY COMPILE_DEFINITIONS CD_HIDE_SUCCESS)
 endif()
 
 option(ColorDebug_HIDE_INFO "Choose if you want to compile with CD_HIDE_INFO" FALSE)
 if(ColorDebug_HIDE_INFO)
-    add_definitions(-DCD_HIDE_INFO)
+    set_property(DIRECTORY ${CMAKE_SOURCE_DIR} APPEND PROPERTY COMPILE_DEFINITIONS CD_HIDE_INFO)
 endif()
 
 option(ColorDebug_HIDE_DEBUG "Choose if you want to compile with CD_HIDE_DEBUG" FALSE)
 if(ColorDebug_HIDE_DEBUG)
-    add_definitions(-DCD_HIDE_DEBUG)
+    set_property(DIRECTORY ${CMAKE_SOURCE_DIR} APPEND PROPERTY COMPILE_DEFINITIONS CD_HIDE_DEBUG)
 endif()
 
