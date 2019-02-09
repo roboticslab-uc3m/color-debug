06dff62a8e7faf10e6cf1f8d8c28628701aea893
Bartek Łukawski <bwmn.peter@gmail.com>
2019-02-09 20:17:50 +0100

    Disable CD colors if NO_COLOR env var is present

--- cmake/ColorDebugOptions.cmake
+++ cmake/ColorDebugOptions.cmake
@@ -1,8 +1,13 @@
 # ColorDebug options
 
+if(DEFINED ENV{NO_COLOR})
+    message(STATUS "NO_COLOR environment variable present, disabling color output.")
+endif()
+
 include(CMakeDependentOption)
 
-option(COLOR_DEBUG_NO_COLOR "Choose if you want to disable colors altogether" OFF)
+cmake_dependent_option(COLOR_DEBUG_NO_COLOR "Choose if you want to disable colors altogether" OFF
+                       "NOT DEFINED ENV{NO_COLOR}" ON)
 
 if(COLOR_DEBUG_NO_COLOR)
     set_property(DIRECTORY ${CMAKE_SOURCE_DIR} APPEND PROPERTY COMPILE_DEFINITIONS CD_NO_COLOR)
