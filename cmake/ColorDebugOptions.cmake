0f7f952c8a2dc41c44514c3a07d08b11b155a5fd
Bartek Łukawski <bwmn.peter@gmail.com>
2019-02-09 14:05:41 +0100

    Add configurable CMake COLOR_DEBUG_NO_COLOR option

--- cmake/ColorDebugOptions.cmake
+++ cmake/ColorDebugOptions.cmake
@@ -1,31 +1,50 @@
 # ColorDebug options
 
-option(COLOR_DEBUG_HIDE_ERROR "Choose if you want to compile with CD_HIDE_ERROR" FALSE)
+include(CMakeDependentOption)
+
+option(COLOR_DEBUG_NO_COLOR "Choose if you want to disable colors altogether" OFF)
+
+if(COLOR_DEBUG_NO_COLOR)
+    set_property(DIRECTORY ${CMAKE_SOURCE_DIR} APPEND PROPERTY COMPILE_DEFINITIONS CD_NO_COLOR)
+endif()
+
+cmake_dependent_option(COLOR_DEBUG_HIDE_ERROR "Choose if you want to hide error level messages" OFF
+                       "NOT COLOR_DEBUG_NO_COLOR" OFF)
+
 if(COLOR_DEBUG_HIDE_ERROR)
     set_property(DIRECTORY ${CMAKE_SOURCE_DIR} APPEND PROPERTY COMPILE_DEFINITIONS CD_HIDE_ERROR)
 endif()
 
-option(COLOR_DEBUG_HIDE_WARNING "Choose if you want to compile with CD_HIDE_WARNING" FALSE)
+cmake_dependent_option(COLOR_DEBUG_HIDE_WARNING "Choose if you want to hide warning level messages" OFF
+                       "NOT COLOR_DEBUG_NO_COLOR" OFF)
+
 if(COLOR_DEBUG_HIDE_WARNING)
     set_property(DIRECTORY ${CMAKE_SOURCE_DIR} APPEND PROPERTY COMPILE_DEFINITIONS CD_HIDE_WARNING)
 endif()
 
-option(COLOR_DEBUG_HIDE_SUCCESS "Choose if you want to compile with CD_HIDE_SUCCESS" FALSE)
+cmake_dependent_option(COLOR_DEBUG_HIDE_SUCCESS "Choose if you want to hide success level messages" OFF
+                       "NOT COLOR_DEBUG_NO_COLOR" OFF)
+
 if(COLOR_DEBUG_HIDE_SUCCESS)
     set_property(DIRECTORY ${CMAKE_SOURCE_DIR} APPEND PROPERTY COMPILE_DEFINITIONS CD_HIDE_SUCCESS)
 endif()
 
-option(COLOR_DEBUG_HIDE_INFO "Choose if you want to compile with CD_HIDE_INFO" FALSE)
+cmake_dependent_option(COLOR_DEBUG_HIDE_INFO "Choose if you want to hide info level messages" OFF
+                       "NOT COLOR_DEBUG_NO_COLOR" OFF)
+
 if(COLOR_DEBUG_HIDE_INFO)
     set_property(DIRECTORY ${CMAKE_SOURCE_DIR} APPEND PROPERTY COMPILE_DEFINITIONS CD_HIDE_INFO)
 endif()
 
-option(COLOR_DEBUG_HIDE_DEBUG "Choose if you want to compile with CD_HIDE_DEBUG" FALSE)
+cmake_dependent_option(COLOR_DEBUG_HIDE_DEBUG "Choose if you want to hide debug level messages" OFF
+                       "NOT COLOR_DEBUG_NO_COLOR" OFF)
+
 if(COLOR_DEBUG_HIDE_DEBUG)
     set_property(DIRECTORY ${CMAKE_SOURCE_DIR} APPEND PROPERTY COMPILE_DEFINITIONS CD_HIDE_DEBUG)
 endif()
 
-option(COLOR_DEBUG_FULL_FILE "Choose if you want to compile with CD_FULL_FILE" FALSE)
+option(COLOR_DEBUG_FULL_FILE "Choose if you want to compile with CD_FULL_FILE" OFF)
+
 if(COLOR_DEBUG_FULL_FILE)
     set_property(DIRECTORY ${CMAKE_SOURCE_DIR} APPEND PROPERTY COMPILE_DEFINITIONS CD_FULL_FILE)
 endif()
